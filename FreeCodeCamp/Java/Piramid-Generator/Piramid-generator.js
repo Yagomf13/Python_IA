7325d263776e3b7a6656869d12a02e44323155b2
Yagomf13 <yagomf13@gmail.com>
2024-04-25 10:58:06 +0200

    piramide

--- FreeCodeCamp/Java/Piramid-Generator/Piramid-generator.js
+++ FreeCodeCamp/Java/Piramid-Generator/Piramid-generator.js
@@ -1,6 +1,24 @@
-let character = 'Hello';
-let count = 8;
-let rows = ['Naomi','Quincy', 'CamperChan']
-console.log(rows[0]);
-rows[2]='10';
-console.log(rows);
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
