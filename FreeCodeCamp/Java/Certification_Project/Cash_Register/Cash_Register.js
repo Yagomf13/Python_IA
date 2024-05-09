e107724a99f4383e85da53aed5b6e3edb903b890
Yagomf13 <yagomf13@gmail.com>
2024-05-09 12:42:02 +0200

    reduce, split, slice, map, sort y filter

--- FreeCodeCamp/Java/Certification_Project/Cash_Register/Cash_Register.js
+++ FreeCodeCamp/Java/Certification_Project/Cash_Register/Cash_Register.js
@@ -1,3 +1,13 @@
+    // const users = [
+    //     { name: 'John', age: 34 },
+    //     { name: 'Amy', age: 20 },
+    //     { name: 'camperCat', age: 10 }
+    //   ];
+    
+    //   const sumOfAges = users.reduce((sum, user) => sum + user.age, 0);
+    //   console.log(sumOfAges);
+  
+
 function checkCashRegister(price, cash, cid) {
     let cambio = cash - price;
     let efectivo = [];
