6c5bee9c78dd14306d87d4ad50c025203dd356db
Your NameYago <yagomf13@gmail.com>
2024-05-10 19:16:57 +0200

    aaa

--- FreeCodeCamp/Java/Certification_Project/Cash_Register/cash.js
+++ FreeCodeCamp/Java/Certification_Project/Cash_Register/cash.js
@@ -1,56 +1,57 @@
-function checkCashRegister(price, cash, cid) {
-    let cambio = cash - price;
-    let dineroDisponible = 0;
-    let cambioEnMonedas = cambio;
-    let devuelta = [];
-    let monedas = {
-        "ONE HUNDRED": [100, 0],
-        "TWENTY": [20, 0],
-        "TEN": [10, 0],
-        "FIVE": [5, 0],
-        "ONE": [1, 0],
-        "QUARTER": [0.25, 0],
-        "DIME": [0.1, 0],
-        "NICKEL": [0.05, 0],
-        "PENNY": [0.01, 0]
-    };
-
-    // Calcular el dinero disponible
-    for (let i = 0; i < cid.length; i++) {
-        dineroDisponible += cid[i][1];
+const coin_value = {
+    "ONE HUNDRED":100,
+    "TWENTY":20.0,
+    "TEN":10.0,
+    "FIVE":5.0,
+    "ONE":1.0,
+    "QUARTER":0.25,
+    "DIME":0.1,
+    "NICKEL":0.05,
+    "PENNY":0.01
     }
-    dineroDisponible = dineroDisponible.toFixed(2);
+  function add(a, b, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round((a + b) * multiplier) / multiplier;
+  }
+  function rest(a, b, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round((a - b) * multiplier) / multiplier;
+  }
+  function checkCashRegister(price, cash, cid) {
+    let change = rest(cash, price, 2);
+    let result = {'status': "INSUFFICIENT_FUNDS", 'change':[]};
+    let total_amount = 0;
 
-    // Verificar si hay suficiente dinero disponible
-    if (cambio > dineroDisponible) {
-        return { status: "INSUFFICIENT_FUNDS", change: [] };
-    } 
-    
-    else if (cambio == dineroDisponible) {
-        return { status: "CLOSED", change: cid };
-    } 
-    
-    else {
-        // Actualizar la cantidad de cada moneda disponible
-        for (const nombreMoneda in monedas) {
-            let cantidadMoneda = cid.find(item => item[0] === nombreMoneda);
-            if (cantidadMoneda) {
-                monedas[nombreMoneda][1] = cantidadMoneda[1];
-            }
-        }
+    for (const coin_type of cid.reverse()){
+      let [coin, total] = coin_type;
+      total_amount = total_amount + total;
+      let amount = Math.round(total/coin_value[coin]);
+      // console.log(change/coin_value[coin], amount)
 
-        // Calcular el cambio en monedas
-        for (const nombreMoneda in monedas) {
-            while (cambioEnMonedas >= monedas[nombreMoneda][0] && monedas[nombreMoneda][1] > 0) {
-                devuelta.push(monedas[nombreMoneda][0]);
-                cambioEnMonedas -= monedas[nombreMoneda][0];
-                monedas[nombreMoneda][1] -= monedas[nombreMoneda][0];
-            }
+      if (change/coin_value[coin]>=1 && amount>0){
+        let add_coin_type = [coin,0];
+        // console.log(change/coin_value[coin], amount)
+
+        while (change/coin_value[coin]>=1 && amount>0){
+          // console.log(change, coin_value[coin], amount)
+          add_coin_type[1] = add(add_coin_type[1], coin_value[coin], 2);
+          change = rest(change, coin_value[coin], 2);
+          amount=amount-1;
         }
-        console.log(monedas, devuelta);
-        
-        return { status: "OPEN", change: devuelta };
+        result['change'].push(add_coin_type);
+      }
+      console.log(total_amount, rest(cash, price, 2));
+      if (change == 0){
+        result['status'] = "OPEN";
+        if (total_amount == cash - price){
+          result['status'] = "CLOSED";
+          result['change'] = cid.reverse();
+        }
+        return result
+      }
     }
+    result['change']=[]
+    return result
 }
 
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
