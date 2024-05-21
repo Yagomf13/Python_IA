8395ae878ccddcdbb3d451bfb753831f5783f126
Your NameYago <yagomf13@gmail.com>
2024-05-21 19:28:13 +0200

    api

--- FreeCodeCamp/JS/Interactive/Certification_Project/Cash_Registrer/script.js
+++ FreeCodeCamp/JS/Interactive/Certification_Project/Cash_Registrer/script.js
@@ -1,62 +1,127 @@
-const cash = document.getElementById("cash")
-const boton = document.getElementById("purchase-btn")
-const cambio = document.getElementById("change-due")
-
-function checkCashRegister(price, cash, cid) {
-    let cambio = cash - price;
-    let dineroDisponible = 0;
-    let cambioEnMonedas = cambio;
-    let devuelta = [];
-    let monedas = {
-        "ONE HUNDRED": [100, 0],
-        "TWENTY": [20, 0],
-        "TEN": [10, 0],
-        "FIVE": [5, 0],
-        "ONE": [1, 0],
-        "QUARTER": [0.25, 0],
-        "DIME": [0.1, 0],
-        "NICKEL": [0.05, 0],
-        "PENNY": [0.01, 0]
-    };
-
-    // Calcular el dinero disponible
-    for (let i = 0; i < cid.length; i++) {
-        dineroDisponible += cid[i][1];
-    }
-    dineroDisponible = dineroDisponible.toFixed(2);
-
-    // Verificar si hay suficiente dinero disponible
-    if (cambio > dineroDisponible) {
-        return { status: "INSUFFICIENT_FUNDS", change: [] };
-    } 
-    
-    else if (cambio == dineroDisponible) {
-        return { status: "CLOSED", change: cid };
-    } 
-    
-    else {
-        // Actualizar la cantidad de cada moneda disponible
-        for (const nombreMoneda in monedas) {
-            let cantidadMoneda = cid.find(item => item[0] === nombreMoneda);
-            if (cantidadMoneda) {
-                monedas[nombreMoneda][1] = cantidadMoneda[1];
+let price = 3.26;
+      let cid = [
+        ['PENNY', 1.01],
+        ['NICKEL', 2.05],
+        ['DIME', 3.1],
+        ['QUARTER', 4.25],
+        ['ONE', 90],
+        ['FIVE', 55],
+        ['TEN', 20],
+        ['TWENTY', 60],
+        ['ONE HUNDRED', 100]
+      ];
+      
+      const displayChangeDue = document.getElementById('change-due');
+      const cash = document.getElementById('cash');
+      const purchaseBtn = document.getElementById('purchase-btn');
+      const priceScreen = document.getElementById('price-screen');
+      const cashDrawerDisplay = document.getElementById('cash-drawer-display');
+      
+      const formatResults = (status, change) => {
+        displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
+        change.map(
+          money => (displayChangeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`)
+        );
+        return;
+      };
+      
+      const checkCashRegister = () => {
+        if (Number(cash.value) < price) {
+          alert('Customer does not have enough money to purchase the item');
+          cash.value = '';
+          return;
+        }
+      
+        if (Number(cash.value) === price) {
+          displayChangeDue.innerHTML =
+            '<p>No change due - customer paid with exact cash</p>';
+          cash.value = '';
+          return;
+        }
+      
+        let changeDue = Number(cash.value) - price;
+        let reversedCid = [...cid].reverse();
+        let denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+        let result = { status: 'OPEN', change: [] };
+        let totalCID = parseFloat(
+          cid
+            .map(total => total[1])
+            .reduce((prev, curr) => prev + curr)
+            .toFixed(2)
+        );
+      
+        if (totalCID < changeDue) {
+          return (displayChangeDue.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>');
+        }
+      
+        if (totalCID === changeDue) {
+          result.status = 'CLOSED';
+        }
+      
+        for (let i = 0; i <= reversedCid.length; i++) {
+          if (changeDue >= denominations[i] && changeDue > 0) {
+            let count = 0;
+            let total = reversedCid[i][1];
+            while (total > 0 && changeDue >= denominations[i]) {
+              total -= denominations[i];
+              changeDue = parseFloat((changeDue -= denominations[i]).toFixed(2));
+              count++;
             }
-        }
-
-        // Calcular el cambio en monedas
-        for (const nombreMoneda in monedas) {
-            while (cambioEnMonedas >= monedas[nombreMoneda][0] && monedas[nombreMoneda][1] > 0) {
-                devuelta.push(monedas[nombreMoneda][0]);
-                cambioEnMonedas -= monedas[nombreMoneda][0];
-                monedas[nombreMoneda][1] -= monedas[nombreMoneda][0];
+            if (count > 0) {
+              result.change.push([reversedCid[i][0], count * denominations[i]]);
             }
+          }
         }
-        console.log(monedas, devuelta);
-        if (cambio > 0) {
-            return {status: "INSUFFICIENT_FUNDS", change: []};
+        if (changeDue > 0) {
+          return (displayChangeDue.innerHTML = '<p>Status: INSUFFICIENT_FUNDS</p>');
         }
-
-        return { status: "OPEN", change: devuelta };
-    }
-}
-
+      
+        formatResults(result.status, result.change);
+        updateUI(result.change);
+      };
+      
+      const checkResults = () => {
+        if (!cash.value) {
+          return;
+        }
+        checkCashRegister();
+      };
+      
+      const updateUI = change => {
+        const currencyNameMap = {
+          PENNY: 'Pennies',
+          NICKEL: 'Nickels',
+          DIME: 'Dimes',
+          QUARTER: 'Quarters',
+          ONE: 'Ones',
+          FIVE: 'Fives',
+          TEN: 'Tens',
+          TWENTY: 'Twenties',
+          'ONE HUNDRED': 'Hundreds'
+        };
+        // Update cid if change is passed in
+        if (change) {
+          change.forEach(changeArr => {
+            const targetArr = cid.find(cidArr => cidArr[0] === changeArr[0]);
+            targetArr[1] = parseFloat((targetArr[1] - changeArr[1]).toFixed(2));
+          });
+        }
+      
+        cash.value = '';
+        priceScreen.textContent = `Total: $${price}`;
+        cashDrawerDisplay.innerHTML = `<p><strong>Change in drawer:</strong></p>
+          ${cid
+            .map(money => `<p>${currencyNameMap[money[0]]}: $${money[1]}</p>`)
+            .join('')}  
+        `;
+      };
+      
+      purchaseBtn.addEventListener('click', checkResults);
+      
+      cash.addEventListener('keydown', e => {
+        if (e.key === 'Enter') {
+          checkResults();
+        }
+      });
+      
+      updateUI();
